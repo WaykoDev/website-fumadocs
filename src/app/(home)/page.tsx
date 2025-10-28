4e307f93f294a3f9d87b258ab1332c72a729982b
franck.chevalier <franck.chevalier@ecole2600.com>
2025-10-28 21:16:21 +0100

    Fix bg color

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import NoScroll from "@/components/NoScroll";
 
 export default function HomePage() {
   return (
-    <main className="relative flex flex-col items-center justify-center h-screen overflow-hidden bg-white text-black transition-colors duration-300 dark:bg-black dark:text-white">
+    <main className="relative flex flex-col items-center justify-center h-screen overflow-hidden text-black transition-colors duration-300 dark:text-white">
       <NoScroll />
       {/* Fond Galaxy */}
       <div className="absolute inset-0 z-0">
